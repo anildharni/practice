5747c6f519c1275752e0339d12de1cb3d7ac45c8
lina_d_inrahd <anildharni370z@gmail.com>
2022-06-05 17:20:31 +0530

    Clean Quiz app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import { useRoutes } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-import ToDoContextProvider from "./store/Context";
-import AllMeetups from "./pages/AllMeetups/AllMeetups";
-import FavMeetups from "./pages/FavMeetups/FavMeetups";
-import NewMeetup from "./pages/NewMeetup/NewMeetupCreate";
-
 function App() {
 
-  const AllMeetupPaths = () => useRoutes([
-    { path: "/", element: <AllMeetups /> },
-    { path: "/allmeetups", element: <AllMeetups /> },
-    { path: "/newmeetup", element: <NewMeetup /> },
-    { path: "/favmeetups", element: <FavMeetups /> },
-  ]);
 
   return (
     <>
-      <ToDoContextProvider>
-        <Layout>
-        </Layout>
-        <AllMeetupPaths />
-      </ToDoContextProvider>
+      <h1>AppJs</h1>
     </>
   );
 }
